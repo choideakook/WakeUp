3bd03e9ea495c783f1e770955570d1515b7bb835
choideakook <shdrnrhd113@naver.com>
2023-12-27 09:20:15 +0900

    Feat: 프론트에서 로그인 전 페이지를 기역하는 기능 구현 실패

--- src/routes/webRouter.js
+++ src/routes/webRouter.js
@@ -2,11 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const { auth } = require('../.credentails/development.json')
+const jwtService = require('../lib/jwt/jwtService')
 
 router.get('/', (req, res) => {
-
-
-    res.render('home/home', auth)
+    res.render('home/home', isLogin(req.headers['Authorization']))
 })
+
+function isLogin(atk) {
+    if(!atk)
+        return {
+            auth: auth,
+            atk: false
+        }
+    else
+        return {
+            user: jwtService.getUser(atk),
+            atk: true
+        }
+}
 
 module.exports = { webRouter: router }
